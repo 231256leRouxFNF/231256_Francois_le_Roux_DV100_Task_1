03e15bc8f4014f0e8e5a8a603a97502cedfb3d26
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2024-06-02 16:26:31 +0200

    Getting there pt2
    Nearly there, just need to change ID's so that there aren't any errors and match them up with the HTML

--- js/script.js
+++ js/script.js
@@ -48,6 +48,51 @@ function loadTickets() {
 
 
 // Code from W6 on how to add more than 1 ticket to the cart
+// // how arrays are defined
+// let numbers = [40, 100, 1];
+// let points = new Array(40, 100 ,1);
+
+// console.log(numbers);
+// console.log(points);
+
+// let good = [40];
+// let bad = new Array(40);
+
+// console.log(good);
+// console.log(bad);
+
+// let dogs = ["golden retriever", "labrador", "staffie", "bulldog", "rotweiller", "toi-pomme"];
+
+// console.log(dogs); //whole array
+// console.log(dogs.length); //just length
+// console.log(dogs[0]); //specific item
+// console.log(dogs[dogs.length-1]); //last item
+// console.log(Array.isArray(dogs)); //is array, or not?
+// console.log(dogs.toString()); 
+// console.log(dogs.join(", "));
+// console.log(dogs.join(" * "));
+
+// dogs.pop();
+// console.log(dogs);
+
+// let dog = dogs.pop();
+// console.log(dog);
+
+
+// dogs.push("toi-pomme");
+// console.log(dogs);
+
+// let newDogs = dogs.slice(1, 4,);
+
+// console.log(newDogs);
+// console.log(dogs);
+
+// dogs.splice(1, 2, "jack russel", "doberman", "pitbull");
+
+// console.log(dogs);
+
+
+// JS for counter thingy
 
 function decreaseMars(){
   if (document.getElementById('ticketsMars').value != 0){
@@ -61,33 +106,31 @@ function increaseMars(){
   document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) + 1;
 }
 
-
 let totalMarsCost = 0;
+let totalSaturnCost = 0;
+let finalCost = 0;
 
 function total(){
   totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 50;
-  document.getElementById('total').innerHTML = "R " + totalMarsCost;
+  totalSaturnCost = parseInt(document.getElementById('ticketsSaturn').value) * 70;
+  finalCost = totalMarsCost + totalSaturnCost;
+  document.getElementById('total').innerHTML = "R " + finalCost;
 
 }
-//////
-function decreaseMars(){
+
+
+//Saturn
+function decreaseSaturn(){
   if (document.getElementById('ticketsSaturn').value != 0){
-      document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsMars').value) - 1;
+      document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) - 1;
   } else {
-      document.getElementById('ticketsMars').value = 0;
+      document.getElementById('ticketsSaturn').value = 0;
   }
 }
 
-function increaseMars(){
-  document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) + 1;
+function increaseSaturn(){
+  document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) + 1;
 }
 
 
-let totalMarsCost = 0;
-
-function total(){
-  totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 50;
-  document.getElementById('total').innerHTML = "R " + totalMarsCost;
-  
-}
 
