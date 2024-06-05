3c2386cffa972e558fc02dd946e8fca685aceaa4
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2024-06-05 09:16:49 +0200

    Getting Pt3
    Almost there

--- js/script.js
+++ js/script.js
@@ -106,18 +106,31 @@ function increaseMars(){
   document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) + 1;
 }
 
-let totalMarsCost = 0;
-let totalSaturnCost = 0;
-let finalCost = 0;
-
-function total(){
-  totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 50;
-  totalSaturnCost = parseInt(document.getElementById('ticketsSaturn').value) * 70;
-  finalCost = totalMarsCost + totalSaturnCost;
-  document.getElementById('total').innerHTML = "R " + finalCost;
-
+//Neptune
+function decreaseNeptune(){
+  if (document.getElementById('ticketsNeptune').value != 0){
+      document.getElementById('ticketsNeptune').value = parseInt(document.getElementById('ticketsNeptune').value) - 1;
+  } else {
+      document.getElementById('ticketsNeptune').value = 0;
+  }
 }
 
+function increaseNeptune(){
+  document.getElementById('ticketsNeptune').value = parseInt(document.getElementById('ticketsNeptune').value) + 1;
+}
+
+//Jupiter
+function decreaseJupiter(){
+  if (document.getElementById('ticketsJupiter').value != 0){
+      document.getElementById('ticketsJupiter').value = parseInt(document.getElementById('ticketsJupiter').value) - 1;
+  } else {
+      document.getElementById('ticketsJupiter').value = 0;
+  }
+}
+
+function increaseJupiter(){
+  document.getElementById('ticketsJupiter').value = parseInt(document.getElementById('ticketsJupiter').value) + 1;
+}
 
 //Saturn
 function decreaseSaturn(){
@@ -132,5 +145,48 @@ function increaseSaturn(){
   document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) + 1;
 }
 
+//Uranus
+function decreaseUranus(){
+  if (document.getElementById('ticketsUranus').value != 0){
+      document.getElementById('ticketsUranus').value = parseInt(document.getElementById('ticketsUranus').value) - 1;
+  } else {
+      document.getElementById('ticketsUranus').value = 0;
+  }
+}
 
+function increaseUranus(){
+  document.getElementById('ticketsUranus').value = parseInt(document.getElementById('ticketsUranus').value) + 1;
+}
 
+//Moon
+function decreaseMoon(){
+  if (document.getElementById('ticketsMoon').value != 0){
+      document.getElementById('ticketsMoon').value = parseInt(document.getElementById('ticketsMoon').value) - 1;
+  } else {
+      document.getElementById('ticketsMoon').value = 0;
+  }
+}
+
+function increaseMoon(){
+  document.getElementById('ticketsMoon').value = parseInt(document.getElementById('ticketsMoon').value) + 1;
+}
+
+let totalMarsCost = 0;
+let totalNeptuneCost = 0;
+let totalJupiterCost = 0;
+let totalSaturnCost = 0;
+let totalUranusCost = 0;
+let totalMoonCost = 0;
+let finalCost = 0;
+
+function total(){
+  totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 25000;
+  totalNeptuneCost = parseInt(document.getElementById('ticketsNeptune').value) * 45000;
+  totalJupiterCost = parseInt(document.getElementById('ticketsJupiter').value) * 65000;
+  totalSaturnCost = parseInt(document.getElementById('ticketsSaturn').value) * 40000;
+  totalUranusCost = parseInt(document.getElementById('ticketsUranus').value) * 50000;
+  totalMoonCost = parseInt(document.getElementById('ticketsMoon').value) * 15000;
+  finalCost = totalMarsCost + totalNeptuneCost + totalJupiterCost + totalSaturnCost + totalUranusCost + totalMoonCost;
+  document.getElementById('total').innerHTML = "R " + finalCost;
+
+}
